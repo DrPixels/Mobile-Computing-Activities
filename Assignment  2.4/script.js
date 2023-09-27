ba8de5251af256c91154284e8533a286f0f73b5d
DrPixels <145904039+DrPixels@users.noreply.github.com>
2023-09-27 22:43:48 +0800

    Add files via upload

---
+++ Assignment  2.4/script.js
@@ -0,0 +1,18 @@
+const createbutton = document.querySelector(".create-button");
+
+let status;
+
+createbutton.addEventListener("click", () => {
+  //For Password
+  const password = document.querySelector(".password").value;
+  const passwordStatus = document.querySelector(".password-status");
+  const confirmpassword = document.querySelector(".confirm-password").value;
+  console.log(password);
+  if (password != confirmpassword) {
+    clearInterval(status);
+    passwordStatus.innerHTML = `<i class="fa-solid fa-triangle-exclamation fa-lg"></i> Password are not the same. Please try again.`;
+    status = setTimeout(() => {
+      passwordStatus.innerHTML = "";
+    }, 2000);
+  }
+});
