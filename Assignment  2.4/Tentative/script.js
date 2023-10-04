a325c54bd1c328979d6f794f30b3146906e915b3
DrPixels <145904039+DrPixels@users.noreply.github.com>
2023-10-05 01:09:57 +0800

    Update script.js

--- Assignment  2.4/Tentative/script.js
+++ Assignment  2.4/Tentative/script.js
@@ -5,22 +5,40 @@ const ageError = document.querySelector(".age-error-div");
 let age;
 let birthDateInput;
 
+//To set the Maximum set of date
+const bdayInput = document.getElementById("birthdate-input");
+let maxYear = new Date().getFullYear() - 18;
+let maxDay = new Date().getDate();
+let maxMonth = new Date().getMonth() + 1;
+let maxBdayInput = `${maxYear}-${maxMonth.toString().padStart(2, "0")}-${maxDay
+  .toString()
+  .padStart(2, "0")}`;
+console.log(maxBdayInput);
+console.log(typeof maxBdayInput);
+bdayInput.max = maxBdayInput;
+
 // For Birthdate Min and Max
 
 const ageDisplay = document.querySelector(".input-age");
 
 function calculateAge() {
   birthDateInput = document.getElementById("birthdate-input").value;
-  let bDay = +new Date(birthDateInput);
+  let bDay = new Date(birthDateInput);
+  let currentDate = new Date();
 
-  age = Math.floor((Date.now() - bDay) / 31556952000);
-  if (age < 0) {
-    age = 0;
-    ageDisplay.value = 0;
-    return;
+  // Calculate the difference in years
+  let age = currentDate.getFullYear() - bDay.getFullYear();
+
+  // Check if the current date's month and day are before the birthdate's month and day
+  if (
+    currentDate.getMonth() < bDay.getMonth() ||
+    (currentDate.getMonth() === bDay.getMonth() &&
+      currentDate.getDate() < bDay.getDate())
+  ) {
+    age--;
   }
 
-  ageDisplay.value = age;
+  ageDisplay.value = `${age || ""}`;
 }
 
 //For the Login and Signup Popup
@@ -76,11 +94,10 @@ function createAccount() {
     return;
   }
 
-  if (signupForm.checkValidity()) {
-    signupForm.classList.add("hide");
-    loginForm.classList.add("hide");
-    successCreate.classList.remove("hide");
-  }
+  signupForm.classList.add("hide");
+  loginForm.classList.add("hide");
+  viewSummaryContainer.classList.add("hide");
+  successCreate.classList.remove("hide");
 }
 
 function signUp() {
@@ -95,52 +112,124 @@ function logIn() {
   viewSummaryContainer.classList.add("hide");
 }
 
-function logInNow() {
-  loginForm.classList.remove("hide");
-  signupForm.classList.add("hide");
-  viewSummaryContainer.classList.add("hide");
-  successCreate.classList.add("hide");
-}
-
 //For View Summary
 function viewSummary() {
   // Document Query Selector of Each Input in Registration Form
+
+  //For Full Name
   const firstName = document.querySelector("#first-name-input").value;
   const lastName = document.querySelector("#last-name-input").value;
   const middleInitial = document.querySelector("#middle-intial-input").value;
+  const fullNameSummaryDiv = document.querySelector(".full-name-summary");
+
+  let fullname;
+  if (!firstName || !lastName || !middleInitial) {
+    fullname = "";
+  } else {
+    fullname = `${firstName} ${middleInitial}. ${lastName}`;
+  }
+
+  //For Username
   const username = document.querySelector("#username-input").value;
+  const usernameSummaryDiv = document.querySelector(".username-summary");
+
+  //For nickname
   const nickname = document.querySelector("#nickname-input").value;
+  const nicknameSummaryDiv = document.querySelector(".nickname-summary");
+
+  //For Email
   const emailAddress = document.querySelector("#emailaddress-input").value;
+  const emailSummaryDiv = document.querySelector(".email-summary");
 
+  //For Birthdate
+  const birthdateSummaryDiv = document.querySelector(".birthdate-summary");
+
+  //For Age
+  const ageSummaryDiv = document.querySelector(".age-summary");
+  let agetemp;
+  if (age) {
+    agetemp = `${age} years old`;
+  }
+
+  //For Birthplace
   const birthplace = document.querySelector("#birthplace-input").value;
-  const gender = document.querySelector("#gender-input").value;
-  const civilStatus = document.querySelector("#civilstatus-input").value;
-  const phonenumber = document.querySelector("#phonenumber-input").value;
-  const landline = document.querySelector("#landline-input").value;
-  const address = document.querySelector("#address-input").value;
+  const birthplaceSummaryDiv = document.querySelector(".birthplace-summary");
 
+  //For Gender
+  const gender = document.querySelector("#gender-input").value;
+  const genderSummaryDiv = document.querySelector(".gender-summary");
+
+  //For Civil Status
+  const civilStatus = document.querySelector("#civilstatus-input").value;
+  const civilSummaryDiv = document.querySelector(".civil-summary");
+
+  //For Phone Number
+  const phonenumber = document.querySelector("#phonenumber-input").value;
+  const phonenumSummaryDiv = document.querySelector(".phonenum-summary");
+
+  //For Landline
+  const landline = document.querySelector("#landline-input").value;
+  const landlineSummaryDiv = document.querySelector(".landline-summary");
+
+  //For Address
+  const address = document.querySelector("#address-input").value;
+  const addressSummaryDiv = document.querySelector(".address-summary");
+
+  //For Father's Information
   const FfirstName = document.querySelector("#Ffirst-name-input").value;
   const FlastName = document.querySelector("#Flast-name-input").value;
   const FmiddleInitial = document.querySelector("#Fmiddle-intial-input").value;
+  const FFullnameSummaryDiv = document.querySelector(
+    ".father-full-name-summary"
+  );
+
+  let Ffullname;
+  if (!FfirstName || !FlastName || !FmiddleInitial) {
+    Ffullname = "";
+  } else {
+    Ffullname = `${FfirstName} ${FlastName}. ${FmiddleInitial}`;
+  }
+
   const Foccupation = document.querySelector("#Foccupation").value;
+  const FoccupationSummaryDiv = document.querySelector(".foccupation-summary ");
 
   const MfirstName = document.querySelector("#Mfirst-name-input").value;
   const MlastName = document.querySelector("#Mlast-name-input").value;
   const MmiddleInitial = document.querySelector("#Mmiddle-intial-input").value;
+
+  const MFullnameSummaryDiv = document.querySelector(
+    ".mother-full-name-summary"
+  );
+  let Mfullname;
+  if (!MfirstName || !MlastName || !MmiddleInitial) {
+    Mfullname = "";
+  } else {
+    Mfullname = `${MfirstName} ${MmiddleInitial}. ${MlastName}`;
+  }
+
   const Moccupation = document.querySelector("#Moccupation").value;
+  const MoccupationSummaryDiv = document.querySelector(".moccupation-summary");
 
   loginForm.classList.add("hide");
   signupForm.classList.add("hide");
   viewSummaryContainer.classList.remove("hide");
 
-  const informationSummaryPar = document.querySelector(
-    ".information-summary-p"
-  );
-  informationSummaryPar.innerHTML = `--- <b>ACCOUNT LOG IN DETAILS</b> --- <br/> <b>Full Name</b>: ${firstName} ${middleInitial}. ${lastName} <br/> <b>Username:</b> ${username} <br/> <b>Nickname:</b> ${nickname} <br/> <b>Email Address:</b> ${emailAddress} <br/> <br/> --- <b>PERSONAL DETAILS</b> --- <br/> <b>Birthdate:</b> ${
-    birthDateInput || ""
-  } <br/> <b>Age:</b> ${
-    age || "unknown"
-  } years old <br/> <b>Birthplace:</b> ${birthplace} <br/> <b>Gender:</b> ${gender} <br/> <b>Civil Status:</b> ${civilStatus} <br/> <b>Phone Number:</b> ${phonenumber} <br/> <b>Landline:</b> ${landline} <br/> <b>Address:</b> ${address} <br/> <br/> --- <b>PARENTS DETAILS</b> --- <br/> <b>Father's Full Name:</b> ${FfirstName} ${FmiddleInitial}. ${FlastName} <br/> <b>Father's Occupation:</b> ${Foccupation} <br/><br/> <b>Mother's Full Name:</b> ${MfirstName} ${MmiddleInitial}. ${MlastName} <br/> <b>Mother's Occupation:</b> ${Moccupation}`;
+  fullNameSummaryDiv.textContent = `${fullname || "N/A"}`;
+  usernameSummaryDiv.textContent = `${username || "N/A"}`;
+  nicknameSummaryDiv.textContent = `${nickname || "N/A"}`;
+  emailSummaryDiv.textContent = `${emailAddress || "N/A"}`;
+  birthdateSummaryDiv.textContent = `${birthDateInput || "N/A"}`;
+  ageSummaryDiv.textContent = `${agetemp || "N/A"}`;
+  birthplaceSummaryDiv.textContent = `${birthplace || "N/A"}`;
+  genderSummaryDiv.textContent = `${gender || "N/A"}`;
+  civilSummaryDiv.textContent = `${civilStatus || "N/A"}`;
+  phonenumSummaryDiv.textContent = `${phonenumber || "N/A"}`;
+  landlineSummaryDiv.textContent = `${landline || "N/A"}`;
+  addressSummaryDiv.textContent = `${address || "N/A"}`;
+  FFullnameSummaryDiv.textContent = `${Ffullname || "N/A"}`;
+  FoccupationSummaryDiv.textContent = `${Foccupation || "N/A"}`;
+  MFullnameSummaryDiv.textContent = `${Mfullname || "N/A"}`;
+  MoccupationSummaryDiv.textContent = `${Moccupation || "N/A"}`;
 }
 
 function exitSummary() {
